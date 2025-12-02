a2603692d721afcd1b2b361d39474541741767bc
Alexmih <147265032+Axm749@users.noreply.github.com>
2025-12-02 23:44:37 +0300

    Update lotok.vue

--- src/components/lotok.vue
+++ src/components/lotok.vue
@@ -2,22 +2,22 @@
   <div>
     <!-- заголовок -->
     <div class="header">
-        <headerTab 
+        <headerTab
             title="Расчет кабельных лотков"
             v-bind:info = "componentInfo"
         ></headerTab>
     </div>
-    
+
     <!-- поля ввода -->
     <v-card class="pa-5" tile>
 
         <h2 class="my-5">Параметры кабеля</h2>
 
-        <div 
+        <div
             v-for="(server, index) in cableParam"
             :key="index"
             class="inLine"
-        >   
+        >
             <div class="inCardTab mr-1 mt-1">
                     <v-text-field
                         v-model="server.title"
@@ -39,7 +39,7 @@
                         </template>
                     </v-text-field>
 
-                    
+
                     <v-text-field
                         v-model="server.count"
                         type="number"
@@ -59,7 +59,7 @@
                         />
                         </template>
                     </v-text-field>
-                    
+
                     <v-text-field
                         v-model="server.diameter"
                         type="number"
@@ -77,19 +77,19 @@
                             title="Диаметр (мм)"
                             desc = "Диаметр одного кабеля данного вида."
                         />
-                        </template> 
+                        </template>
                     </v-text-field>
-                    
+
                     <v-btn
                         color="error"
                         @click="deleteItem(server, index)"
                         class="mt-5 mx-auto mb-2"
                         width="100%"
-                    >  
-                        <v-icon>mdi-trash-can</v-icon>  удалить 
+                    >
+                        <v-icon>mdi-trash-can</v-icon>  удалить
                     </v-btn>
                 </div>
-            
+
 
         </div>
 
@@ -97,7 +97,7 @@
             <v-col>
                 <v-btn
                     color=""
-                    @click="cabel_reset"
+                    @click="cable_reset"
                     width="100%"
                 >
                     Сбросить все кабели
@@ -115,38 +115,45 @@
             <v-col>
                 <v-btn
                     color=""
-                    @click="cabel_reset"
+                    @click="cable_reset"
                     width="100%"
                 >
                     <v-icon>mdi-cog</v-icon>
                     Дополнительные настройки
                 </v-btn>
             </v-col>
-            
 
-            
+
+
         </v-row>
 
-        
 
 
 
 
 
 
-      <v-btn 
-          @click="start" 
+
+      <v-btn
+          @click="start"
           color="primary"
           class="mt-5"
           width="100%"
       >Старт</v-btn>
+		<v-btn
+			@click="secondary"
+			color="primary"
+			class="mt-5"
+			width="100%"
+		>второй старт</v-btn>
+
   </v-card>
 
   <!-- уведомление об ошибке -->
   <v-snackbar
           v-model="snackbar"
           :timeout="timeout"
-          
+
       >
       {{ errorText }}
 
@@ -162,7 +169,7 @@
       </template>
       </v-snackbar>
 
-  <v-card 
+  <v-card
       class="pa-5 mt-5"
       v-show="started"
   >
@@ -170,14 +177,14 @@
        <p>{{answer}}</p>
 <!--	  <td>Длина кабельного лотка: {{answer.Lotok_Shirota}} мм.</td>-->
 <!--	  <td> Высота кабельного лотка: {{answer.Lotok_Visota}} мм</td>-->
-      
+
       <v-btn
         @click="started=false"
         class="mt-2"
       >скрыть</v-btn>
  </v-card>
 
- 
+
 </div>
 </template>
 
@@ -229,31 +236,31 @@ export default{
         componentInfo: {
             name: 'расчет кабельных лотков',
             incomes: [
-            'Название кабелей;', 
-            'Диаметры кабелей (мм);', 
-            'Кол-во кабелей одного вида (шт.);', 
+            'Название кабелей;',
+            'Диаметры кабелей (мм);',
+            'Кол-во кабелей одного вида (шт.);',
             'запас диаметра для прокладки (мм).',
             ],
             outcomes: [
-            'Размеры лотка, необходимые для прокладки кабеля;'], 
+            'Размеры лотка, необходимые для прокладки кабеля;'],
             description: `В этом разделе программы рассчитываются габариты лотка, необходимого для прокладки указанных кабелей.`,
             disclaimers: [
             'Модуль рассчитывает в двух режимах работы: ',
             'В прямоугольном, лоток будет параметры ширины и высоты;',
             'В круговом, труба имеет значение радиуса.',
             "_",
-            
+
             ],
             constants: [
             {
-                name: 'PlaceHolder 1 value', 
-                massive: [ 
+                name: 'PlaceHolder 1 value',
+                massive: [
                     'PlaceHolder 1 value'
                 ]
             },
             {
-                name: 'PlaceHolder 2 value', 
-                massive: [ 
+                name: 'PlaceHolder 2 value',
+                massive: [
                     'PlaceHolder 2 value'
                 ]
             },
@@ -270,6 +277,7 @@ export default{
 			this.answer.Lotok_Shirota = 0
 			// Простой счёт лотка
 			this.cableParam.forEach(item => {
+				// console.log(item)
 				if (max_height < item.diameter) {
 					max_height = item.diameter
 				}
@@ -286,12 +294,12 @@ export default{
 					Optimization_Arr_1.push(
 						{
 							id: id_counter,
-							cabel_width: Number(item.diameter),
-							cabel_height: Number(item.diameter),
-							cabels_used: [{
+							cable_width: Number(item.diameter),
+							cable_height: Number(item.diameter),
+							cables_used: [{
 								id: id_counter,
-								cabel_width: Number(item.diameter),
-								cabel_height: Number(item.diameter),
+								cable_width: Number(item.diameter),
+								cable_height: Number(item.diameter),
 							}]
 						}
 					)
@@ -299,10 +307,10 @@ export default{
 				}
 			})
 			// Небольшой костыль, сортирует от большего к меньшему для улучшеной работы алгоритма
-			Optimization_Arr_1.sort((a, b) => b.cabel_height - a.cabel_height)
+			Optimization_Arr_1.sort((a, b) => b.cable_height - a.cable_height)
 			console.log(Optimization_Arr_1,'sorted')
 			// Массив использованных кабелей
-			let used_cabels = []
+			let used_cables = []
 			// Параметр для определения какую ширину взять, по сути костыль?
 			let more_wider = 0
 			let less_wider = 0
@@ -312,58 +320,58 @@ export default{
 			Optimization_Arr_1.forEach(Loop_1 => {
 				Optimization_Arr_1.forEach(Loop_2 => {
 					// console.log(Loop_1,' ', Loop_2)
-					if ((Loop_1.cabel_height + Loop_2.cabel_height <= max_height)
-						&& (Loop_1 != Loop_2)
-						&& (!used_cabels.includes(Loop_1))
-						&& (!used_cabels.includes(Loop_2))) {
+					if ((Loop_1.cable_height + Loop_2.cable_height <= max_height)
+						&& (Loop_1 !== Loop_2)
+						&& (!used_cables.includes(Loop_1))
+						&& (!used_cables.includes(Loop_2))) {
 						// выбор используемой ширины
-						if (Loop_1.cabel_width > Loop_2.cabel_width){
-							more_wider = Number(Loop_1.cabel_width)
-							less_wider = Number(Loop_2.cabel_width)
+						if (Loop_1.cable_width > Loop_2.cable_width){
+							more_wider = Number(Loop_1.cable_width)
+							less_wider = Number(Loop_2.cable_width)
 						} else {
-							more_wider = Number(Loop_2.cabel_width)
-							less_wider = Number(Loop_1.cabel_width)
+							more_wider = Number(Loop_2.cable_width)
+							less_wider = Number(Loop_1.cable_width)
 						}
 						console.log(Loop_2,Loop_1)
 						buffer_obj = {
 							id: id_counter,
-							cabel_width: more_wider,
-							cabel_height: Number(Loop_1.cabel_height + Loop_2.cabel_height),
+							cable_width: more_wider,
+							cable_height: Number(Loop_1.cable_height + Loop_2.cable_height),
 							// использованные кабели для суммирования данного стака
-							cabels_used: [],
+							cables_used: [],
 							// не использованное пространство
 							unspace_width: (more_wider - less_wider),
 							unspace_height: less_wider
 						}
 						// заполнение пустого пространства
 						Optimization_Arr_1.forEach(Loop_3 => {
-							if ((Loop_3.cabel_height <= buffer_obj.unspace_height)
-								&& (Loop_3.cabel_width <= buffer_obj.unspace_width)
-								&& (Loop_3 != buffer_obj)
-								&& (Loop_3 != Loop_2)
-								&& (Loop_3 != Loop_1)
-								&& (!used_cabels.includes(Loop_3))) {
+							if ((Loop_3.cable_height <= buffer_obj.unspace_height)
+								&& (Loop_3.cable_width <= buffer_obj.unspace_width)
+								&& (Loop_3 !== buffer_obj)
+								&& (Loop_3 !== Loop_2)
+								&& (Loop_3 !== Loop_1)
+								&& (!used_cables.includes(Loop_3))) {
 								// Уменьшаем неиспользованное пространство
-								buffer_obj.unspace_width = buffer_obj.unspace_width - Loop_3.cabel_width
+								buffer_obj.unspace_width = buffer_obj.unspace_width - Loop_3.cable_width
 								// Добавляем кабели для заполнения в стак в массив использованных кабелей
-								Loop_3.cabels_used.forEach(put_used_cables => {
-									buffer_obj.cabels_used.push(put_used_cables)
+								Loop_3.cables_used.forEach(put_used_cables => {
+									buffer_obj.cables_used.push(put_used_cables)
 								})
-								used_cabels.push(Loop_3)
+								used_cables.push(Loop_3)
 							}
 						})
 						// добавление использованных кабелей для создания стака
-						Loop_1.cabels_used.forEach(put_used_cables => {
-							buffer_obj.cabels_used.push(put_used_cables)
+						Loop_1.cables_used.forEach(put_used_cables => {
+							buffer_obj.cables_used.push(put_used_cables)
 						})
-						Loop_2.cabels_used.forEach(put_used_cables => {
-							buffer_obj.cabels_used.push(put_used_cables)
+						Loop_2.cables_used.forEach(put_used_cables => {
+							buffer_obj.cables_used.push(put_used_cables)
 						})
 						// записываем созданый объект в массив оптимизации
                         Optimization_Arr_1.push(buffer_obj)
 						// записываем в использованные кабели, нужно для *удаления* использованных ПРОМЕЖУТОЧНЫХ элементов
-						used_cabels.push(Loop_1)
-						used_cabels.push(Loop_2)
+						used_cables.push(Loop_1)
+						used_cables.push(Loop_2)
 						id_counter += 1
 						// Optimization_Arr_1.splice(Optimization_Arr_1.indexOf(Loop_1),1)
 						// Optimization_Arr_1.splice(Optimization_Arr_1.indexOf(Loop_2),1)
@@ -371,17 +379,168 @@ export default{
 				})
 			})
 			// удаляем использованные кабели из результата
-			used_cabels.forEach(ToDelete => {
+			used_cables.forEach(ToDelete => {
 				Optimization_Arr_1.splice(Optimization_Arr_1.indexOf(ToDelete),1)
 			})
 			this.answer = Optimization_Arr_1
 			console.log(Optimization_Arr_1, 'Optimization_Arr_1 result')
-			console.log(used_cabels, 'cabels used to make stacks')
+			console.log(used_cables, 'cables used to make stacks')
 			// length = length - this.gap_reserve
 			// this.answer.Lotok_Visota = Number(max_height)
 			// this.answer.Lotok_Shirota = Number(length)
 
 		},              //Ф-ция старт
+
+
+		// альтернативная версия алгоритма подсчета лотков
+		/**
+		 * @return answer {
+		 *     width: float,
+		 *     height: float,
+		 *     S: float,
+		 *     cable_list: [{}]
+		 * }
+		 */
+		secondary() {
+			this.started = true
+			var max_height = 0
+			var length = 0
+			// Простой счёт лотка (все в шеренгу)
+			this.cableParam.forEach(item => {
+				if (max_height < item.diameter) {
+					max_height = item.diameter
+				}
+				length = length + (Number(item.diameter * item.count) + (this.gap_reserve * Number(item.count)))
+			})
+			console.log(max_height, 'max height')
+			console.log(length, 'length')
+
+			// Массив для оптимизации
+			let main_arr = []
+			// Счетчик id
+			var id_counter = 0
+			// Создание массива с учётом формы записи кабелей из фронта (т.е. учет параметра count)
+			this.cableParam.forEach(item => {
+				for (let i = 0; i < item.count; i++) {
+					main_arr.push(
+						{
+							id: id_counter,
+							cable_width: Number(item.diameter),
+							cable_height: Number(item.diameter),
+							unused_width: 0,
+							unused_height: 0,
+							cables_used: [{
+								id: id_counter,
+								cable_width: Number(item.diameter),
+								cable_height: Number(item.diameter)
+							}],
+
+						}
+					)
+					id_counter += 1
+				}
+			})
+			console.log(main_arr, 'main arr')
+
+			var j_forbidden = [],
+				new_obj={},
+				new_arr=[],
+				possible_S = 0,
+				current_S = 0
+
+
+
+			// Оптимизация - можем ли мы наложить кабели друг на друга
+			for (let i = 0; i < main_arr.length; i++) {
+				// обнуляем важные переменные
+				j_forbidden = []
+				// console.log(i, 'i')
+
+				// ВАЖНО! Я здесь начинаю второй цикл не с нуля, а со значения первого цикла,
+				// чтобы более мелкие объекты не смотрели на более крупные, что уже позади.
+				// Ведь эта работа бессмысленна
+				for (let j = i; j < main_arr.length; j++) {
+					console.log(i, 'i', j, 'j')
+					// обнуляем служебные переменные
+					new_obj={}
+					new_arr=[]
+					possible_S = 0
+
+					if (j_forbidden.includes(j)){
+						console.log(j, 'is in', j_forbidden, 'must skip')
+					}
+					if ( i === j ){
+						console.log('Dont stack a cable with himself')
+					}
+					// 	i - кого совмещаем _ main_arr[i]
+					// 	j - с кем совмещаем _ main_arr[j]
+
+					// объединяем два кабеля друг на друга при одном условии:
+					// - площадь лотка становится меньше или остаётся неизменной (possible_S <= current_S)
+					// для проведения подсчетов предполагаем, что изменение уже произошло
+					new_obj = this.make_new_arr(main_arr, i, j);
+					new_arr = new_obj.new_arr;
+
+					// потенциальная площадь
+					possible_S = new_obj.new_S;
+
+					// текущая площадь
+					current_S = this.make_S(main_arr)
+
+					if (possible_S < current_S) {
+						console.log('hello', possible_S <= current_S)
+						console.log('hello', possible_S, current_S)
+						main_arr=new_arr
+						// 	также нужно указать, что индекс j в таком случае использовать нельзя
+						j_forbidden.push(j)
+						console.log('j_forbidden', j_forbidden)
+					} else {
+						console.log("didn't stack items:", main_arr[i], main_arr[j])
+					}
+				}
+			}
+
+
+		},
+		// совместить два кабеля и составить новый массив из них
+		/**
+		 *
+		 * @param main_arr
+		 * @param add_to_id
+		 * @param add_id
+		 *
+		 * @return {
+		 *          new_arr: [],
+		 *          max_height: float,
+		 *          total_width: float,
+		 *          new_S: float
+		 *          }
+		 */
+		make_new_arr(new_arr, add_to_id, add_id) {
+			console.log(new_arr, add_to_id, add_id)
+			// здесь и лежит главная логика объединения двух элементов
+			// мы имеем лишь два объекта: new_arr[add_to_id] и new_arr[add_id]
+			// if(
+			//
+			// ){
+			// 	new_arr[add_to_id] = {
+			//
+			// 	}
+			// }
+
+
+
+			return {
+				new_arr: new_arr,
+				max_height: 1,
+				total_width: 2,
+				new_S: this.make_S(new_arr)
+			}
+		},
+
+
+
+
 		deleteItem: function (item, index) {
 			if(this.cableParam[index] === item) {
 			// The template passes index as the second parameter to avoid indexOf,
@@ -394,7 +553,7 @@ export default{
 			this.cableParam.splice(found, 1)
 			}
 		},
-		cabel_reset: function(){
+		cable_reset: function(){
 			this.cableParam = [
 				{id: 0, title: `Широкий`, count: 1, diameter: 15},
 				{id: 1, title: `Тонкий`, count: 1, diameter: 5},
@@ -402,62 +561,27 @@ export default{
 			]
 			// По сути это костыль, лучше как-то отменить мутацию defaultcableParam от изменения cableParam
 			console.log("Hiihihihihihiii!!!11")
+		},
+
+		// расчет площади для массива кабелей
+		make_S(main_arr) {
+			var current_width = 0
+			main_arr.forEach(item => {
+				current_width += item.cable_width
+			})
+			console.log(current_width, 'current width')
+
+			var current_height = 0
+			main_arr.forEach(item => {
+				if (current_height < item.cable_height) {current_height = item.cable_height}
+			})
+			console.log(current_height, 'current height')
+
+			var current_S = current_width*current_height;
+			console.log(current_S, 'current S')
+			return current_S;
 		}
-
-
-        /**
-            // Box_Info.Wide_info input panel, ну короче в vuetify закинешь строчку на ввод данных для широты лотка
-            //  Box_Info.Height_Info input panel, то же самое для высоты лотка
-                Cabel_Laying_Scheme = [[],[]]
-                // Двойной массив, по сути матрица уложения кабеля
-                Box_Info = { Wide_Info: 20, Height_Info: 5 }
-                // Информацая о размерах в миллиметрах !
-                Cabel_Info = [ { Cabel_Name: 'Шаурма 1', Cabel_Inner_Diameter: 5 }, { Cabel_Name: 'Шаурма 2', Cabel_Inner_Diameter: 5 }, { Cabel_Name: 'Шаурма 3', Cabel_Inner_Diameter: 5 }, { Cabel_Name: 'Шаурма 465', Cabel_Inner_Diameter: 5 } ]
-                // Информацая о размерах в миллиметрах ! Диаметр кабеля с учетом изоляции...? Я не оч уверен но вроде да.
-                Total_row_sum = 0
-                // Сумма длин диаметров для одного слоя
-                Height_Modifier = 0
-                // По сути выбор уровня слоя, на котором будут расположены кабели
-                for (let i = 0; i<Cabel_Info.length; i++){
-                    if (Box_Info.Wide_Info >= Cabel_Info[i].Cabel_Inner_Diameter + 5) {
-                        if (Box_Info.Wide_Info > Total_row_sum && Box_Info.Wide_Info >= Total_row_sum + Cabel_Info[i].Cabel_Inner_Diameter + 5){
-                            Total_row_sum = Total_row_sum + Cabel_Info[i].Cabel_Inner_Diameter + 5
-                            Cabel_Laying_Scheme[Height_Modifier].push(Cabel_Info[i])
-                            console.log(Cabel_Laying_Scheme)
-                            console.log(Total_row_sum)
-                        } else {
-                            console.log("Can't fit more in a row!")
-                            Height_Modifier = Height_Modifier + 1
-                            Total_row_sum = 0
-                        }
-                    } else {
-                        console.log("This won't fit!")
-                    }
-                    // if (Box_Info.Wide_Info > Cabel_Info[i].Cabel_Inner_Diameter) {
-                    //     console.log("It fits")
-                    // }
-                }
-            // !!Баг, при переключении уровней теряется кабель!! Как починить пока не знаю
-         */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    },
+	},
       mounted(){
           console.log('hello')
       }
